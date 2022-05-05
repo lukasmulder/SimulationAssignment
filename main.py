fddd395d61ea27624879b2766b4f6e668193e7e2
Lukas Mulder <39460488+lukasmulder@users.noreply.github.com>
2022-05-05 16:20:04 +0200

    first files

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
-print("hello world")
+eventQ = []
 
-#comment
+def setup():
+    eventQ.append( arrival(Pois dist) )
+
+Event = (time, type)
+
+eventhandler(event):
+    graph aanpassen
+
+
+main():
+    init
+    while eventQ not empty:
+        pop event
+        run event
+
+TODO:
+event handler
+event class
+state
+    graph
+    parking lots
+init
+main
+statistics bijhouden
