b24b900e3b4a9c9a721705851e43a25895d4b9d9
Catalinablom <75936151+Catalinablom@users.noreply.github.com>
2022-05-28 15:03:42 +0200

    graph intervals

--- statistics.py
+++ statistics.py
@@ -282,5 +282,24 @@ def comparison_with_standard(standard, data, confidence):
 
     return intervals
 
+def plot_confidence_intervals(intervals, horizontal_line_width = 0.25, color = '#2187bb'):
+    x=0
+    names =[]
+    for name,interval in intervals:
+        x+=1
+        names.append(name)
+        bottom,top = interval
+        mean = (right + left)/2
+        left = x - horizontal_line_width / 2
+        right = x + horizontal_line_width / 2
+        plt.plot([x, x], [top, bottom], color=color)
+        plt.plot([left, right], [top, top], color=color)
+        plt.plot([left, right], [bottom, bottom], color=color)
+        plt.plot(x, mean, 'o', color='#f44336')
+        
+    plt.xticks(range(1,len(intervals)+1), names)
+    plt.title("Confidence Intervals")
 
+    
+    
             
